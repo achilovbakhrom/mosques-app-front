a4858c6e2ca7d75b5392c5c8ef2408cfe9d9616a
mailbak36@gmail.com <mailbak36@gmail.com>
2024-10-09 23:16:50 +0500

    url fix

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -12,7 +12,7 @@ class AuthApi {
       baseURL:
         process.env.NODE_ENV === "development"
           ? "http://localhost:8000/api"
-          : "https://muslim-api.nss.uz",
+          : "https://muslim-api.sies.uz",
     });
     const response = await axiosInstance.post("/user/login/", arg);
     return response.data;
